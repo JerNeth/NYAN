8d5f294e80fdc223ce2e04ab742555d30f1b2d41
Jeremias Neth <Jeremias.neth@gmail.com>
2021-11-19 03:57:30 +0100

    Update GTest fetchcontent to reference main instead of master branch

--- vendor/GTest.cmake
+++ vendor/GTest.cmake
@@ -1,9 +1,8 @@
-cmake_minimum_required (VERSION 3.14)
 include(FetchContent)
 FetchContent_Declare(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG        master
+    GIT_TAG        main
     GIT_SHALLOW    true
 )
 FetchContent_MakeAvailable(googletest)
